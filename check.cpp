83b4ff5e097dca22c23f2647850c1fd3630f5162
Deepak Jha <jhakumardeepakk@gmail.com>
2025-10-22 01:30:36 +0530

    assignments

---
+++ check.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+// using namespace std;
+int main(){
+    int num;
+    // cout<< "Hello World\n";
+    // cout<< 2*3;
+    // cout<<"\n";
+    std::cout<<"Enter a number to print:->\n";
+    std::cin>>num;
+    std::cout<<"Number entered was:- "<<num;
+    return 0;
+}
